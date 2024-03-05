58f5e1ceaa7bb7b8ce0e488d6504b62627de71be
Konstantin Krumin <konstantin.krumin@gmail.com>
2024-03-05 00:20:39 -0500

    basic confifg of server to work with typescript

---
+++ server/index.ts
@@ -0,0 +1,26 @@
+import express, { Request, Response, Application } from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+//For env File
+dotenv.config();
+
+const API_PREFIX = "/api/v1";
+
+const app: Application = express();
+const port = process.env.PORT || 8000;
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (_: Request, res: Response) => {
+  res.send("Welcome to Express & TypeScript Server");
+});
+
+app.get(`${API_PREFIX}/healthcheck`, (_, res) => {
+  return res.status(200).json({ message: "Server is running!" });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
